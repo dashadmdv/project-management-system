37bb5ac8ea7775b90afd753b4ffaa6ce01bbd25d
dashadmdv <apushkins7@gmail.com>
2023-09-15 21:05:12 +0300

    add: CLI project interface (create, add&complete contract)

--- src/cli.py
+++ src/cli.py
@@ -16,8 +16,8 @@ class CLI:
         # here the database is created
         self.controller = CLIController()
 
+        prompt = "I want to work with 1 - projects, 2 - contracts"
         while True:
-            prompt = "I want to work with 1 - projects, 2 - contracts"
             entity_choice = self.input_with_check(prompt, ['1', '2'], back_mode=False)
             if self.always_available_operations(entity_choice):
                 self.controller.close_database()
@@ -41,6 +41,19 @@ class CLI:
             else:
                 return value
 
+    # so that we can input id = 8 or 9
+    def input_id_with_check(self, prompt: str, values_list: list, back_mode=True):
+        prompt += (", or also _8 (with _ !) - show projects list" + ", _9 (with _ !) - show contracts list" +
+                   (self.back_prompt if back_mode else self.exit_prompt)) + ": "
+        values_list.extend(['_8', '_9', '0'])
+        while True:
+            value = input(prompt)
+            if value not in values_list:
+                print("No such option! Try again!")
+                continue
+            else:
+                return value
+
     def always_available_operations(self, choice):
         if choice in ['8', '9', '0']:
             match choice:
@@ -51,8 +64,68 @@ class CLI:
                 case '0':
                     return True
 
+    def always_available_operations_input_id(self, choice):
+        if choice in ['_8', '_9', '0']:
+            match choice:
+                case '_8':
+                    self.show_projects_list()
+                case '_9':
+                    self.show_contracts_list()
+                case '0':
+                    return True
+        else:
+            return True
+
     def project_dialog(self):
-        pass
+        prompt = "Choose an option 1 - create project, 2 - add a contract to the project, 3 - complete the contract"
+        while True:
+            project_action_choice = self.input_with_check(prompt, ['1', '2', '3'])
+            if self.always_available_operations(project_action_choice):
+                break
+            match project_action_choice:
+                case '1':
+                    print(self.controller.add_project())
+                case '2':
+                    project_id = None
+                    contract_id = None
+                    while True:
+                        while True:
+                            project_id = self.input_id_with_check("Select project to add to (ENTER ID)",
+                                                                  self.controller.get_projects_ids())
+                            if self.always_available_operations_input_id(project_id):
+                                if self.controller.count_active_contracts_by_project(project_id) == 1:
+                                    print("This project already has an active contract! Choose another one!")
+                                    continue
+                                break
+                        if project_id == '0':
+                            break
+                        while True:
+                            contract_id = self.input_id_with_check("Select contract to add to (ENTER ID)",
+                                                                   self.controller.get_contracts_ids())
+                            if self.always_available_operations_input_id(contract_id):
+                                break
+                        break
+                    if project_id and contract_id:
+                        print(self.controller.add_contract_to_project(contract_id, project_id))
+                case '3':
+                    project_id = self.input_id_with_check("Select project that has the needed contract (ENTER ID)",
+                                                          self.controller.get_projects_ids())
+                    if not project_id:
+                        continue
+
+                    print(self.controller.get_all_contracts_by_project(project_id))
+                    contracts_list = self.show_contracts_list(
+                        source=self.controller.get_all_contracts_by_project(project_id))
+                    if not contracts_list:
+                        continue
+                    contract_id = self.input_id_with_check("Select contract to complete (ENTER ID)",
+                                                           [str(x[0]) for x in contracts_list])
+                    if not contract_id:
+                        continue
+                    if self.controller.get_contract(contract_id)[-2] == 'completed':
+                        print("The contract is already completed! Choose another one!")
+                        continue
+                    print(self.controller.change_contract_status(contract_id, 'completed'))
 
     def contract_dialog(self):
         pass
@@ -64,11 +137,15 @@ class CLI:
         table.add_rows(projects_list)
         print(table)
 
-    def show_contracts_list(self):
-        contracts_list = self.controller.get_contracts_list()
+    def show_contracts_list(self, source=None):
+        contracts_list = source if source is not None else self.controller.get_contracts_list()
         table = PrettyTable()
         f = " (yyyy-mm-dd)"
         table.field_names = ["Contract id", "Name", f"Date of creation{f}",
                              f"Date of approval{f}", "Status", "In project"]
         table.add_rows(contracts_list)
-        print(table)
+        if not contracts_list:
+            print("There are no contracts!")
+        else:
+            print(table)
+        return contracts_list
